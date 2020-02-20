a3b86bf2748167834f4246848ab1f7b92159cf95
lilients <svantje.lilienthal@tib.eu>
2020-02-20 14:38:15 +0100

    new favicon

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@ includes/footer.php
       <div class="footer">
         <ul class="list-inline">
            <li><a href="mailto:rosi.project(at)tib.eu">Contact</a></li>
-           <li><li><a href="https://www.tib.eu/en/service/data-protection/">Data protection</a></li>
+           <li><a href="https://www.tib.eu/en/service/data-protection/">Data protection</a></li>
            <li><a href="https://labs.tib.eu/info/en/imprint/">Imprint</a></li>
            <li><a href="https://github.com/TIBHannover/rosi-registry">Code (GitHub)</a></li>
          </ul>
